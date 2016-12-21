66b71b62c44c4189a9f6b732d88506b363b31c3d
pc\David <chulded@gmail.com>
2016-12-20 22:33:53 -0500

    creacion del ejercicio #2

--- taller1.py
+++ taller1.py
@@ -7,3 +7,12 @@ root = Tk()
 etiqueta1=Label(root, text="Hello Tkinter..!!")
 etiqueta1.pack()
 root.mainloop()
+
+
+print("\nEJERCICIO #2")
+master=Tk()
+whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it. \n(Mahatma Gandhi)"
+msg=Message(master, text=whatever_you_do)
+msg.config(bg='lightgreen', font=('times',24, 'italic'))
+msg.pack()
+mainloop()
