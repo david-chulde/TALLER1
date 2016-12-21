9cc3a250d0081ccebb6eadda98470765dd67f00b
pc\David <chulded@gmail.com>
2016-12-20 22:52:31 -0500

    creacion del ejercicio#5

--- taller1.py
+++ taller1.py
@@ -30,3 +30,26 @@ class App:
         self.slogan.pack(side=LEFT)
     def write_slogan(self):
             print ("Estamos aprendiendo a usar Tkinter!")
+
+print("\nEJERCICIO #4")
+root = Tk()
+app = App(root)
+root.mainloop()
+
+root = Tk()
+v = IntVar()
+Label(root,
+      text="""Choose a programming language:""",
+      justify = LEFT,
+      padx = 20).pack()
+Radiobutton(root,
+            text="Python",
+            padx = 20, variable=v,
+            value=1).pack(anchor=W)
+Radiobutton(root,
+            text="Perl",
+            padx = 20,
+            variable=v,
+            value=2).pack(anchor=W)
+mainloop()
+
