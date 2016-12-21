b4e7e512faefa0c6df2641906b43de60fc2c0267
pc\David <chulded@gmail.com>
2016-12-20 23:37:15 -0500

    creacion del ejercicio #8

--- taller1.py
+++ taller1.py
@@ -121,3 +121,20 @@ Checkbutton(master, text="Hombre", variable=var1).grid(row=0, sticky=W)
 var2 = IntVar()
 Checkbutton(master, text="Mujer", variable=var2).grid(row=1, sticky=W)
 master.mainloop()
+
+
+print("EJERCICIO #8")
+from tkinter import*
+master = Tk()
+def var_states():
+    print (" hombre: ",var1.get())
+    print ("mujer: ",var2.get())
+Label(master, text="Indicar el sexo:").grid(row=0, sticky=W)
+var1 = IntVar()
+Checkbutton(master, text="Hombre", variable=var1).grid(row=1,sticky=W)
+var2 = IntVar()
+Checkbutton(master, text="Mujer", variable=var2).grid(row=2,sticky=W)
+Button(master, text='Quit', command=master.quit).grid(row=3,sticky=W, padx=4)
+Button(master, text='Show', command=var_states).grid(row=4,sticky=W, pady=4)
+master.mainloop()
+
