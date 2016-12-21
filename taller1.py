8605b3546962066c62bfa157ccea11ef57d8d248
pc\David <chulded@gmail.com>
2016-12-20 22:29:42 -0500

    creacion del primer ejercicio

---
+++ taller1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+print("TALLER #1")
+print("Autor: DAVID CHULDE")
+
+print("\nEJERCICIO #1")
+root = Tk()
+etiqueta1=Label(root, text="Hello Tkinter..!!")
+etiqueta1.pack()
+root.mainloop()
