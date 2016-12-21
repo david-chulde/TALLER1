63b002d7adc0bc832f69033e0170c46045e9cab1
pc\David <chulded@gmail.com>
2016-12-20 23:44:45 -0500

    realizacion del ejercicio #9

--- taller1.py
+++ taller1.py
@@ -138,3 +138,16 @@ Button(master, text='Quit', command=master.quit).grid(row=3,sticky=W, padx=4)
 Button(master, text='Show', command=var_states).grid(row=4,sticky=W, pady=4)
 master.mainloop()
 
+
+print("EJERCICIO #9")
+
+from tkinter import*
+master=Tk()
+Label(master, text='First Name').grid(row=0)
+Label(master, text='Laste Name').grid(row=1)
+e1=Entry(master)
+e2=Entry(master)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+mainloop()
+
