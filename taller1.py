829ba35cfe9e2a284f51c93682e376732109b3e0
pc\David <chulded@gmail.com>
2016-12-20 22:44:29 -0500

    creacion del ejercicio #3

--- taller1.py
+++ taller1.py
@@ -16,3 +16,17 @@ msg=Message(master, text=whatever_you_do)
 msg.config(bg='lightgreen', font=('times',24, 'italic'))
 msg.pack()
 mainloop()
+
+
+print("\nEJERCICIO #3")
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.button = Button(frame, text="SALIR",
+    fg="red",command=frame.quit)
+        self.button.pack(side=LEFT)
+        self.slogan =Button(frame,text="ENTRAR",command=self.write_slogan)
+        self.slogan.pack(side=LEFT)
+    def write_slogan(self):
+            print ("Estamos aprendiendo a usar Tkinter!")
